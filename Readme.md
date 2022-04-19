1a6c3a45bcc92ecf3b592ba43e30d46698fb51be
Edilson Jesus <edilsondevelop@gmail.com>
2022-04-19 20:07:20 -0300

    Iniciando o capitulo 02

--- Readme.md
+++ Readme.md
@@ -4,14 +4,21 @@
 
 ## O foco desse curso é Herança de Orientação a Objetos 
 
-
+### O curso segue uma linha de raciocinio, implementar uma nova funcionalidade!
+Para isso iremos reaproveitar o código que ja foi escrito, com herança de funções já criadas, 
+dando enfanse na manutenção do código.
 
 -   - -  -  - - - - -  - - -- ------------------------------------------
 
+#### Capitulo 01 - O que foi aprendidido
+ - Relembramos conceitos de classe, Propriedades,
+ - Boas práticas na hora de declarar atributos
+ - Declarando sempre as propriedades como sendo privadas inicialmente
+ - Problemas de duplicação de código
+ - Problemas de código extremamente compartilhado
 
-###  O que aprendi no capitulo  04 
 
- ####  Contrutores e estáticos
+
 
 - Construtores
 - Encapsulamento
--- index.js
+++ index.js
@@ -2,14 +2,19 @@ console.log("Bem vindo Ao BytBank \n\n");
 
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import {ContaPoupanca} from "./ContaPoupanca.js";
+import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo",11122243456);
 const cliente2 = new Cliente("Alice", 990088776612);
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(150);
+const contaPoupanca = new Conta(500, cliente1, 1001);
 
-const contaPoupanca = new ContaPoupanca(500, cliente1, 1001)
-
+console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);
+
+
 
