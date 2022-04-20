75b8e115e38c9ee40ec05ee4611f73d6c2aec3ef
Edilson Jesus <edilsondevelop@gmail.com>
2022-04-19 22:13:54 -0300

    Inicio capitulo 02

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,55 +1,13 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export  class ContaCorrente {
-    static numeroConta = 0; 
-    agencia;
-    _cliente;
-     //#saldo = 0     
-    //Atriibuto privado
-    _saldo = 0;
-
-
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
-            this._cliente = novoValor;
-        }    
-    }
-    
-    get cliente(){
-        return this._cliente
-    }
-
-   
-
-    get saldo(){
-        return this._saldo;
-    }
-
+export  class ContaCorrente  extends Conta{
+    static numeroConta = 0;     
     constructor(cliente, agencia){
-        this.cliente = cliente;
-        this.agencia = agencia; 
+        super(0,cliente, agencia);
         ContaCorrente.numeroConta += 1;
     }
 
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-            return;
-        }
-
-        this._saldo += valor;
-    }
-
-    transferir(valor, conta){
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-    }
+   
 
 }
--- index.js
+++ index.js
@@ -8,10 +8,10 @@ import {Conta} from "./Conta.js";
 const cliente1 = new Cliente("Ricardo",11122243456);
 const cliente2 = new Cliente("Alice", 990088776612);
 
-const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(150);
-const contaPoupanca = new Conta(500, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);
